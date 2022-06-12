6cac6fb4054dc5086fe75eb5d9ebc1ec4a08b4a2
Roman Triputko <roman.triputko@gmail.com>
2022-06-12 22:38:51 +0300

    fixes css class name collisions

--- frameworks-mc-example/packages/container/src/App.js
+++ frameworks-mc-example/packages/container/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header/Header'
 import { MarketingApp } from './components/MarketingApp'
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'container',
+})
+
 export const App = () => {
     return (
-        <BrowserRouter>
-            <Header />
-            <MarketingApp />
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <Header />
+                <MarketingApp />
+            </BrowserRouter>
+        </StylesProvider>
     )
 }
