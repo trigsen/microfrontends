806f7463ff79d6482b01b1fc9fcff00c84432ea7
Roman Triputko <roman.triputko@gmail.com>
2022-06-12 22:10:12 +0300

    fixes pipeline

--- frameworks-mc-example/packages/container/src/App.js
+++ frameworks-mc-example/packages/container/src/App.js
@@ -4,7 +4,7 @@ import { MarketingApp } from './components/MarketingApp'
 export const App = () => {
     return (
         <>
-            <h1>container test12345</h1>
+            <h1>container test123456</h1>
             <hr />
             <MarketingApp />
         </>
