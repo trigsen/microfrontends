0fa0852f4d63fc91cb0fb09af06381845e57cfb4
Roman Triputko <roman.triputko@gmail.com>
2022-06-12 20:46:22 +0300

    fixes pipeline

--- frameworks-mc-example/packages/marketing/src/components/Landing.js
+++ frameworks-mc-example/packages/marketing/src/components/Landing.js
@@ -14,7 +14,7 @@ import { Link } from 'react-router-dom';
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {'Coopyright © '}
       <MaterialLink component={Link} to="/" color="inherit">
         Your Website
       </MaterialLink>{' '}
